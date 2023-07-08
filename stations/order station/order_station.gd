0486f3f3059e244c17f23974c75072bc5fd0115d
Abby Smith <asillylittlecreature@gmail.com>
2023-07-07 23:47:10 -0400

    Renamed cards to orders

--- stations/order station/order_station.gd
+++ stations/order station/order_station.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+@onready var main = get_parent()
+
 var customer = preload("res://customer.tscn")
-var card = preload("res://card.tscn")
+var order = preload("res://order.tscn")
 
 const DOOR_POS = Vector2(960, 448)
 const COUNTER_POS = Vector2(384, 448)
@@ -24,4 +26,7 @@ func spawn_customer():
 
 #function to take a customer's order
 func take_order(orderer):
+	#$Camera2D.enabled = true
+	#var new_order = order.instatiate()
+	#main.populate_order(new_order)
 	pass
