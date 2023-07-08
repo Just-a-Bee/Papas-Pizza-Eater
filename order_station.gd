1f0cd4573812099fc8b0585a7f9ddb7d444cbee4
Abby Smith <asillylittlecreature@gmail.com>
2023-07-07 20:14:41 -0400

    Added order station where customers orders are taken Added customer scene, when instanced it moves from the door to the counter and shows its order button Taking the order does nothing yet

---
+++ order_station.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var customer = preload("res://customer.tscn")
+var card = preload("res://card.tscn")
+
+const DOOR_POS = Vector2(960, 448)
+const COUNTER_POS = Vector2(384, 448)
+
+func _ready():
+	spawn_customer()
+
+#function to spawn a new customer at the door
+func spawn_customer():
+	var new_customer = customer.instantiate()
+	new_customer.position = DOOR_POS
+	add_child(new_customer)
+
+#function to take a customer's order
+func take_order(orderer):
+	pass
