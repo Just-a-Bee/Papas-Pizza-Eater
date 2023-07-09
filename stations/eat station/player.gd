8dd71a50896709e858d460a1773aa6f8943a54dd
Abby Smith <asillylittlecreature@gmail.com>
2023-07-08 23:31:01 -0400

    Moved player stats into globals to make them easier to change

--- stations/eat station/player.gd
+++ stations/eat station/player.gd
@@ -1,26 +1,20 @@
 extends AnimatedSprite2D
 class_name Player
 
-var speed = 100
-var acceleration = 50
-var direction = 0
-var turn_speed = PI/2
-var slow_turn_speed = PI/8
-
 func _process(delta):
 	#handle moving
 	var moving = false
 	if Input.is_action_pressed("up"):
-		position += Vector2(0, -speed).rotated(rotation)*delta
+		position += Vector2(0, -Globals.crawl_speed).rotated(rotation)*delta
 		moving = true
 		play("Crawl")
 	else:
 		pause()
 	
 	#handle turning
-	var turn_tmp = turn_speed
+	var turn_tmp = Globals.turn_speed
 	if moving:
-		turn_tmp = slow_turn_speed
+		turn_tmp = Globals.slow_turn_speed
 	if Input.is_action_pressed("left"):
 		rotation -= turn_tmp*delta
 	if Input.is_action_pressed("right"):
