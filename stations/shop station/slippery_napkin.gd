3173d5b1835717e9355daf1c3ead30fa97c009e6
Abby Smith <asillylittlecreature@gmail.com>
2023-07-30 11:47:14 +0100

    Added 2 more shop items, upgrades to the slppery napkin

--- stations/shop station/slippery_napkin.gd
+++ stations/shop station/slippery_napkin.gd
@@ -3,3 +3,7 @@ extends ShopItem
 func do_effect():
 	Globals.crawl_speed += 30
 	self.hide()
+	if name == "SlipperyNapkin":
+		get_parent().get_node("SlipperierNapkin").show()
+	if name == "SlipperierNapkin":
+		get_parent().get_node("SlipperiestNapkin").show()
